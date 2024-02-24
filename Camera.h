a5ebd6776ae3c41b80e98ef1d078932b9c9c9913
Mc-StArT <alesha@gilenko.net>
2024-02-24 18:50:10 +0300

    Uart-related funcions moved out of Camera class

--- Camera.h
+++ Camera.h
@@ -3,7 +3,6 @@
 
 #include "Registers.h"
 
-
 enum ImageSize
 {
     _160x120,
@@ -25,36 +24,30 @@ class Camera
 public:
     Camera(ImageSize imageSize = ImageSize::_160x120, Colorspace colorspace = Colorspace::RGB555);
 
-    void setCameraImageSize(ImageSize imageSize); // REVIEW
-    void setCameraColorspace(Colorspace colorspace); // REVIEW 
+    void setCameraImageSize(ImageSize imageSize);    // REVIEW
+    void setCameraColorspace(Colorspace colorspace); // REVIEW
 
     bool setCameraRegister(const RegisterData *regData, bool ensure = false); // REVIEW
-    bool setCameraRegister(uint8_t addr, uint8_t val, bool ensure); // REVIEW 
-    void readCameraRegister(RegisterData *regData); // REVIEW
+    bool setCameraRegister(uint8_t addr, uint8_t val, bool ensure);           // REVIEW
+    void readCameraRegister(RegisterData *regData);                           // REVIEW
 
     uint8_t getCameraRegister(RegisterData *regData); // REVIEW
-    uint8_t getCameraRegister(uint8_t addr); // REVIEW
+    uint8_t getCameraRegister(uint8_t addr);          // REVIEW
 
     bool setCameraRegisters(const RegisterData *regs, int retries = 2); // TODO
 
-    void resetCamera(); // REVIEW 
+    void resetCamera(); // REVIEW
 
     bool setCameraPresaler(int prescaler); // REVIEW - why did I use int here?
 
     bool setCameraPLLMultiplier(uint8_t multiplier); // REVIEW - unused in most scenarios
 
-    void waitForPCLKRising(int16_t = 0); // TODO
-    void waitForPCLKFalling(int16_t = 0); // TODO
-    void waitForHREFRising(int16_t = 0); // TODO
-    void waitForHREFFalling(int16_t = 0); // TODO
-    void waitForVSYNCRising(int16_t = 0); // TODO
+    void waitForPCLKRising(int16_t = 0);   // TODO
+    void waitForPCLKFalling(int16_t = 0);  // TODO
+    void waitForHREFRising(int16_t = 0);   // TODO
+    void waitForHREFFalling(int16_t = 0);  // TODO
+    void waitForVSYNCRising(int16_t = 0);  // TODO
     void waitForVSYNCFalling(int16_t = 0); // TODO
-
-    void uartWaitForPreviousByteToBeSent(); // TODO
-    void uartSendByte(uint8_t); // TODO
-    bool uartIsReady(); // TODO
-    uint8_t uartHasData(); // TODO
-    uint8_t uartGetData(); // TODO
 
 private:
     ImageSize _imageSize;
@@ -64,4 +57,10 @@ private:
     uint16_t _width, _height
 };
 
-#endif
+void uartWaitForPreviousByteToBeSent(); // TODO
+void uartSendByte(uint8_t);             // TODO
+bool uartIsReady();                     // TODO
+uint8_t uartHasData();                  // TODO
+uint8_t uartGetData();                  // TODO
+
+#endif /* CAMERA_H_ */
